60f1ce098f4635f8612f85227cb4505a6dbf97a1
Bruno Moreira Batista <brunobatysta@gmail.com>
2021-08-12 15:26:55 -0300

    widget

---
+++ ProjetosDart/agenda_crud/android/app/src/main/kotlin/br/com/brunobatysta/agenda_crud/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.brunobatysta.agenda_crud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ProjetosDart/agenda_crud/lib/app/view/contact_form.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ContactForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Cadastro de Contato")));
+  }
+}
