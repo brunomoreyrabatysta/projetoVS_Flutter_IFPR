f97eb452340d2d35940f6bce39e29e44b8a5acd7
Bruno Moreira Batista <brunobatysta@gmail.com>
2021-09-24 15:42:26 -0300

    persistência local

--- ProjetosDart/agenda_crud/lib/main.dart
+++ ProjetosDart/agenda_crud/lib/main.dart
@@ -5,3 +5,19 @@ import 'app/my_app.dart';
 void main() {
   runApp(MyApp());
 }
+
+
+/*
+voi main() async {
+  // await/async avisa para aguardar a finalização do método, para continuar
+  await BuscarDados();
+  print('carrega a tela');
+  print('botões');
+  print('campos');
+  print('imagens');
+}
+
+Future buscarDados() {
+  return Future.delayed(Duration(seconds:5),() => print('carregou os dados... '));
+}
+*/
